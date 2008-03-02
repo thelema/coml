7ce0b4a70b13e01ce14bc6751d37186308b7b010
Eric Norige <thelema314@gmail.com>
2008-03-01 21:43:20 -0600

    Fix headers to have proper license info

--- coml.ml
+++ coml.ml
@@ -1,12 +1,28 @@
-(* dispimg - A very simple image viewer.
+(* coml - LablGTK-based Comic-book (CBR,CBZ) viewer 
    
-   Written by Shawn Wagner (shawnw@speakeasy.org) and released into the
-   public domain.
+   (original dispimg source) Written by Shawn Wagner (shawnw@speakeasy.org) 
+   and released into the public domain.
+
+    Copyright (C) 2008 Eric Norige
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
    
    Compile with: 
    
    % make
-   % make install
    
  *)
 
